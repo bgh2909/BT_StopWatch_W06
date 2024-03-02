422b62bee5f73a6c60cae2096017007e319e4fec
bgh2909 <bgh2909@gmail.com>
2024-03-02 20:55:13 +0700

    Add project files.

---
+++ ConsoleApp6/FileName.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal class StopWatch
+    
+        {
+            private DateTime startTime;
+            private DateTime endTime;
+
+            public DateTime StartTime => startTime;
+            public DateTime EndTime => endTime;
+
+            public StopWatch()
+            {
+                startTime = DateTime.Now;
+            }
+
+            public void Start()
+            {
+                startTime = DateTime.Now;
+            }
+
+            public void Stop()
+            {
+                endTime = DateTime.Now;
+            }
+
+            public DateTime GetStartTime()
+            {
+                return startTime;
+            }
+            public DateTime GetEndTime()
+            {
+                return endTime;
+            }
+            public double GetElapseTime()
+            {
+                /* double startTicks = startTime.Millisecond;
+                double endTicks = endTime.Millisecond;
+                return endTicks - startTicks; */
+                return endTime.Millisecond - startTime.Millisecond;
+
+            }
+        }
+    }
+
---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            int[] arr = new int[100000];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = (int)rnd.Next();
+            }
+            StopWatch sw = new StopWatch();
+
+            sw.Start();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int minCurrent = arr[i];
+                int minIndex = i;
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (minCurrent > arr[j])
+                    {
+                        minCurrent = arr[j];
+                        minIndex = j;
+                    }
+
+                }
+                int temp = arr[minIndex];
+                arr[minIndex] = arr[i];
+                arr[i] = temp;
+            }
+            sw.Stop();
+
+            foreach (int a in arr)
+            {
+                Console.WriteLine(a);
+            }
+
+            sw.GetElapseTime();
+
+            Console.WriteLine("Elapsed time: " + sw.GetElapseTime() + " milliseconds");
+            
+            Console.ReadKey();
+        }
+    }
+}
